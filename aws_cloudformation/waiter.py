46f47a4c16290f03eed6416e9a3e28876eb03ee4
Sanhe Hu <husanhe@gmail.com>
2022-12-07 21:06:33 -0500

    change waiter logging

--- aws_cloudformation/waiter.py
+++ aws_cloudformation/waiter.py
@@ -37,7 +37,7 @@ class Waiter:
                     sys.stdout.write(
                         f"\r{self.tab}on {attempt} th attempt, "
                         f"elapsed {elapsed} seconds, "
-                        f"remain {int(remaining)} seconds ..."
+                        f"remain {self.timeout - elapsed} seconds ..."
                     )
                     sys.stdout.flush()
                 yield attempt, int(elapsed)
